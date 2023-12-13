79dcf7ccd77ccb5ddb7abad636db2222c59d359e
JL <JL>
2023-12-13 12:41:25 -0800

    add SHA3

--- ChecksumPy.py
+++ ChecksumPy.py
@@ -1,9 +1,11 @@
-from hashlib import sha512, sha256, sha1, md5
+from hashlib import sha3_512, sha512, sha3_256, sha256, sha1, md5
 from tkinter import messagebox
 import sys
 
 def CalcHashes(bits):
-    str = "SHA-512: " + sha512(bits).hexdigest().upper() + "\n\n"
+    str = "SHA3-512: " + sha3_512(bits).hexdigest().upper() + "\n\n"
+    str += "SHA-512: " + sha512(bits).hexdigest().upper() + "\n\n"
+    str += "SHA3-256: " + sha3_256(bits).hexdigest().upper() + "\n\n"
     str += "SHA-256: " + sha256(bits).hexdigest().upper() + "\n\n"
     str += "SHA-1: " + sha1(bits).hexdigest().upper() + "\n\n"
     str += "MD5: " + md5(bits).hexdigest().upper()
