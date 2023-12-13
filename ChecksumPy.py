2cae592d82876b754b93f9c5dddbbf39f509ccd0
JL <JL>
2023-12-13 12:22:03 -0800

    make uppercase

--- ChecksumPy.py
+++ ChecksumPy.py
@@ -3,10 +3,10 @@ from tkinter import messagebox
 import sys
 
 def CalcHashes(bits):
-    str = "SHA-512: " + sha512(bits).hexdigest() + "\n"
-    str += "SHA-256: " + sha256(bits).hexdigest() + "\n"
-    str += "SHA-1: " + sha1(bits).hexdigest() + "\n"
-    str += "MD5: " + md5(bits).hexdigest()
+    str = "SHA-512: " + sha512(bits).hexdigest().upper() + "\n\n"
+    str += "SHA-256: " + sha256(bits).hexdigest().upper() + "\n\n"
+    str += "SHA-1: " + sha1(bits).hexdigest().upper() + "\n\n"
+    str += "MD5: " + md5(bits).hexdigest().upper()
     return str
     
 path = sys.argv[1]
