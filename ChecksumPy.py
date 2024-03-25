7239324d47fed16f72823ecaa6960b45babcea39
JL <JL>
2024-03-25 14:26:45 -0700

    Add CRC32

--- ChecksumPy.py
+++ ChecksumPy.py
@@ -1,15 +1,17 @@
 from hashlib import sha3_512, sha512, sha3_256, sha256, sha1, md5
+from zlib import crc32
 from tkinter import messagebox
 import sys
 
 def CalcHashes(bits):
-    str = "SHA3-512: " + sha3_512(bits).hexdigest().upper() + "\n\n"
-    str += "SHA-512: " + sha512(bits).hexdigest().upper() + "\n\n"
-    str += "SHA3-256: " + sha3_256(bits).hexdigest().upper() + "\n\n"
-    str += "SHA-256: " + sha256(bits).hexdigest().upper() + "\n\n"
-    str += "SHA-1: " + sha1(bits).hexdigest().upper() + "\n\n"
-    str += "MD5: " + md5(bits).hexdigest().upper()
-    return str
+    strg = "SHA3-512: " + sha3_512(bits).hexdigest().upper() + "\n\n"
+    strg += "SHA-512: " + sha512(bits).hexdigest().upper() + "\n\n"
+    strg += "SHA3-256: " + sha3_256(bits).hexdigest().upper() + "\n\n"
+    strg += "SHA-256: " + sha256(bits).hexdigest().upper() + "\n\n"
+    strg += "SHA-1: " + sha1(bits).hexdigest().upper() + "\n\n"
+    strg += "MD5: " + md5(bits).hexdigest().upper() + "\n\n"
+    strg += "CRC32: " + str(crc32(bits)) 
+    return strg
     
 path = sys.argv[1]
 file = open(path, "rb")
